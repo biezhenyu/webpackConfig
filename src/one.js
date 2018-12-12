c21fb995054a42e22ed32acbef5f8dc88addefab
biezhenyu <biezhenyu521@163.com>
2018-12-12 15:11:53 +0800

    提交

--- src/one.js
+++ src/one.js
@@ -1,2 +1,4 @@
 import jquery from 'jquery';
+import lodash from 'lodash';
+import react from 'react';
 console.log('one1');
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ const WebpackMd5Hash = require('webpack-md5-hash');
 module.exports = {
   entry: {
     one: "./src/one.js",
-    two: "./src/two.js"
+    two: "./src/two.js",
+    common: ['jquery', 'lodash'],
+    react: ['react']    // 用于抽取react
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -24,7 +26,12 @@ module.exports = {
         commons: {
           name: "commons",
           chunks: "initial",
-          minChunks: 2
+          minChunks: Infinity  // 无限次
+        },
+        react: {
+          name: 'react',
+          chunks: "initial",
+          minChunks: Infinity  // 无限次
         }
       }
     },
